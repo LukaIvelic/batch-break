b70a637092e7051c34438de6a43b2457a473fe17
Luka Ivelić <ivelicluka@gmail.com>
2026-01-16 00:24:15 +0100

    fix: matching vercel deployment requests

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,3 @@
 export * from "./services/auth/AuthService";
 export * from "./services/users/UsersService";
-export { API_BASE_URL, ENDPOINTS, apiRequest } from "./config";
+export { API_BASE_URL, endpoints, api } from "./config";
